9e06fd3f93e2f7b6951bd62a64f30ca0aba42764
Petreius <pabelaube@hotmail.fr>
2014-10-15 22:04:55 +0200

    Update MutationAleatoireRoutage.java

--- TSP/MutationAleatoireRoutage.java
+++ TSP/MutationAleatoireRoutage.java
@@ -1,4 +1,4 @@
-// Cette classe définit la mutation aléatoire. Il s'agit d'inverser deux noeuds dans la suite des noeuds visités.
+// Cette classe dÃ©finit la mutation aleatoire. Il s'agit d'inverser deux noeuds dans la suite des noeuds visites.
 
 public class MutationAleatoireRoutage {
 
@@ -8,37 +8,41 @@ public class MutationAleatoireRoutage {
         int randIndex1 = (int) (newSolution.tailleRoute() * Math.random());
         int randIndex2 = (int) (newSolution.tailleRoute() * Math.random());
 
-        // On récupère les noeuds à cet index
+        // On rÃ©cupÃ¨re les noeuds Ã  cet index
         Noeud swapNoeud1 = newSolution.getNoeud(randIndex1);
         Noeud swapNoeud2 = newSolution.getNoeud(randIndex2);
 
-        // On échange ces noeuds dans le parcours.
+        // On Ã©change ces noeuds dans le parcours.
         newSolution.setNoeud(randIndex2, swapNoeud1);
         newSolution.setNoeud(randIndex1, swapNoeud2);
 	}
 	
 public static void twoOptMove(Routage newSolution)
-	{//on opère de la même manière sauf que l'on change le chemin de sens(dans le while)
+	{//on opere de la meme maniere sauf que l'on change le chemin de sens (dans le while)
 	// On choisit deux positions du parcours au hasard.
+	int i;
+	int j;
 	int randIndex1 = (int) (newSolution.tailleRoute() * Math.random());
 	int randIndex2 = (int) (newSolution.tailleRoute() * Math.random());
-
+    // j > i
 	if(randIndex1>randIndex2){
-		int i = randIndex2;
-		int j = randIndex1;		
+		i = randIndex2;
+		j = randIndex1;		
 	}
 	else{
-		int i = randIndex1;
-		int j = randIndex2;
+		i = randIndex1;
+		j = randIndex2;
 	}
 	while(i<j){
-			// On récupère les noeuds à ces indexes
+			// On recupere les noeuds a ces indexs
 		Noeud swapNoeud1 = newSolution.getNoeud(i);
 		Noeud swapNoeud2 = newSolution.getNoeud(j);
-		// On échange ces noeuds dans le parcours.
+		// On echange ces noeuds dans le parcours.
 		newSolution.setNoeud(j, swapNoeud1);
 		newSolution.setNoeud(i, swapNoeud2);
 		i++;
 		j--;
 }
+}
+
 }
